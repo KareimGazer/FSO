8fb2fb10841f2eadce8d26777cdc683cdc9e2a71
KareimGazer <kareimgazar1990@gmail.com>
2024-06-21 01:43:39 +0300

    phonebook completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   const [persons, setPersons] = useState([{ id: 1, name: 'Arto Hellas', number: '040-123456' }]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const handelSubmit = (event) => {
     event.preventDefault()
@@ -15,7 +16,7 @@ const App = () => {
       alert('Please enter a name and number')
       return
     }
-    if(persons.find(person => person.name === newName)) {
+    if(persons.find(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`)
       return
     }
@@ -33,13 +34,15 @@ const App = () => {
     setNewNumber('')
   }
 
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <>
       <Dashboard title={'Phonebook'}>
-        <Filter></Filter>
+        <Filter term={searchTerm} onFilter={setSearchTerm}></Filter>
         <PersonForm newName={newName} onNameChange={setNewName} newNumber={newNumber} onNumberChange={setNewNumber} onSubmit={handelSubmit}/>
       </Dashboard>
-      <PeopleList title={'Your List of Contacts'} people={persons}/>
+      <PeopleList title={'Your List of Contacts'} people={filteredPersons}/>
     </>
   )
 }
--- part2/phonebook/src/Filter.jsx
+++ part2/phonebook/src/Filter.jsx
@@ -1,5 +1,13 @@
-const Filter = ({term, items, onFilter}) => {
-    
+const Filter = ({term, onFilter}) => {
+
+    return (
+        <label htmlFor="search-bar">
+            search for:
+            <input id="search-bar" type="text" value={term} onChange={(event) => onFilter(event.target.value)}/>
+            <button type="submit">search</button>
+        </label>
+
+    )
 }
 
 export default Filter;
